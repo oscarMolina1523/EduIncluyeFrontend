ef91f8fac3ed1e2bee17e833301099384336f9c6
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-04 11:57:42 -0600

    adding video autoplay

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,43 @@
+import React, { useState, useEffect } from "react";
 import { View, StyleSheet, Text } from "react-native";
+import WebView from "react-native-webview";
+import YoutubePlayer from "react-native-youtube-iframe";
 
 const HomeScreen = () => {
+  const [playing, setPlaying] = useState(false);
+  const [muted, setMuted] = useState(true);
+
+  // Forzar autoplay después de 1 segundo
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setPlaying(true);
+    }, 1000);
+    return () => clearTimeout(timeout);
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Categorias</Text>
-      <View></View>
+      {/* <YoutubePlayer
+        height={200}
+        play={playing}
+        videoId="wVuDj9V3olo"
+        onReady={() => setPlaying(true)} // Activa play cuando el player esté listo
+        initialPlayerParams={{
+          mute: muted,
+          controls: true,
+        }}
+      /> */}
+      <WebView
+        style={{ height: 200 }}
+        javaScriptEnabled={true}
+        domStorageEnabled={true}
+        allowsInlineMediaPlayback={true}
+        mediaPlaybackRequiresUserAction={false}
+        source={{
+          uri: "https://www.youtube.com/embed/wVuDj9V3olo?autoplay=1&mute=1&controls=0&loop=1&playlist=wVuDj9V3olo",
+        }}
+      />
     </View>
   );
 };
@@ -19,7 +52,7 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     backgroundColor: "#F5F5F5",
     paddingHorizontal: 12,
-    paddingTop:12,
+    paddingTop: 12,
   },
   title: {
     fontSize: 24,
