6b2d5be00b97878b9907ab11e03979295412b514
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-03 19:38:42 -0600

    creating base of home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,14 +2,32 @@ import { View, StyleSheet, Text } from "react-native";
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>this is the home view</Text>t
+    <View style={styles.container}>
+      <Text style={styles.title}>Categorias</Text>
+      <View></View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    flexDirection: "column",
+    textAlign: "center",
+    justifyContent: "flex-start",
+    backgroundColor: "#F5F5F5",
+    paddingHorizontal: 12,
+    paddingTop:12,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+    textAlign: "left",
+    color: "black",
+  },
 });
 
 export default HomeScreen;
