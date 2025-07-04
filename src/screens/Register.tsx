2d44ee35e63ebf5be4888a9ff0074ce2a66ef011
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-03 19:33:08 -0600

    creating register view

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,15 +1,140 @@
-import { View, Text, StyleSheet } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
+import { RootStackParamList } from "../routes/Navigation";
 
 const RegisterScreen = () => {
-  return (
-    <View>
-      <Text>this is the register view</Text>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {},
-});
+    const navigation =
+      useNavigation<NativeStackNavigationProp<RootStackParamList, "Login">>();
+  
+    return (
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.container}>
+          <Image
+            source={{
+              uri: "https://i.ibb.co/LyxKpZD/logo.png",
+            }}
+            style={{
+              width: "100%",
+              height: "20%",
+              alignItems: "center",
+              objectFit: "contain",
+            }}
+          />
+          <Text style={styles.title}>Bienvenido a EDU-INCLUYE!</Text>
+          <View>
+            <Text style={styles.text}>Usuario</Text>
+            <TextInput
+              keyboardType="email-address"
+              style={styles.input}
+              placeholder="ejemplo@gmail.com"
+            />
+          </View>
+          <View>
+            <Text style={styles.text}>Contraseña</Text>
+            <TextInput
+              secureTextEntry={true}
+              style={styles.input}
+              placeholder="password"
+            />
+          </View>
+          <View>
+            <Text style={styles.text}>Confirmar contraseña</Text>
+            <TextInput
+              secureTextEntry={true}
+              style={styles.input}
+              placeholder="confirm password"
+            />
+          </View>
+          <View>
+            <TouchableOpacity style={styles.button}>
+              <Text style={styles.buttonText}>Registrarse</Text>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.footer}>
+            <Text>¿Ya tienes una cuenta?</Text>
+            <Text
+              style={styles.link}
+              onPress={() => navigation.navigate("Login")}
+            >
+              Iniciar Sesión
+            </Text>
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+    );
+  };
+  
+  const styles = StyleSheet.create({
+    container: {
+      width: "100%",
+      height: "100%",
+      flex: 1,
+      flexDirection: "column",
+      textAlign: "center",
+      justifyContent: "center",
+      backgroundColor: "#F5F5F5",
+      paddingHorizontal: 12,
+    },
+    text: {
+      fontSize: 14,
+      fontWeight: "bold",
+      marginBottom: 14,
+      textAlign: "left",
+      color: "black",
+    },
+    title: {
+      fontSize: 24,
+      fontWeight: "bold",
+      marginBottom: 20,
+      textAlign: "center",
+      color: "black",
+    },
+    input: {
+      width: "100%",
+      height: "auto",
+      borderWidth: 1,
+      borderColor: "#ccc",
+      borderRadius: 8,
+      padding: 10,
+      marginBottom: 15,
+      backgroundColor: "rgba(255, 255, 255, 0.9)",
+    },
+    button: {
+      backgroundColor: "#339999",
+      borderRadius: 8,
+      paddingVertical: 10,
+      paddingHorizontal: 20,
+      width: "100%",
+      height: 50,
+      alignItems: "center",
+    },
+    buttonText: {
+      color: "#FFFFFF",
+      fontSize: 20,
+      fontWeight: "bold",
+    },
+    footer: {
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      marginTop: 10,
+    },
+    link: {
+      color: "blue",
+      marginLeft: 5,
+    },
+  });
 
 export default RegisterScreen;
