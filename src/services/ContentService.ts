96e8a99ea3a5f7e5771ee1df675256ad86ba6f1c
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-07-05 19:05:18 -0600

    adding methods to control content

---
+++ src/services/ContentService.ts
@@ -0,0 +1,44 @@
+import ContentModel from "../models/ContentModel";
+import HTTPService from "./http-service";
+
+export default class ContentService extends HTTPService {
+  private path: string;
+  constructor() {
+    super();
+    this.path = "content";
+  }
+
+  async getAll() {
+    const data = await super.get(this.path);
+    return data.map((item: any) => ContentModel.fromJson(item));
+  }
+
+  async getById(id: string) {
+    const item = await super.get(`${this.path}/${id}`);
+    if (!item) return null;
+
+    return ContentModel.fromJson(item);
+  }
+
+  async addContent(content: ContentModel) {
+    //el body que se va mandar se construye
+    const body = ContentModel.fromJsonModel(content).toJsonDTO();
+    //devuelte null o la categoria creada
+    const result = await super.post(`${this.path}`, body);
+    if (!result) return null;
+    //retorna la categoria creada es decir el resultado
+    return ContentModel.fromJson(result);
+  }
+
+  async updateContent(id: string, content: ContentModel) {
+    const body = ContentModel.fromJsonModel(content).toJsonDTO();
+    const json = await super.put(`${this.path}/${id}`, body);
+    if (!json) return null;
+
+    return ContentModel.fromJson(json);
+  }
+
+  async deleteCategory(id: string) {
+    await super.delete(`${this.path}/${id}`);
+  }
+}
